11aa376b8b0ec169efa3cc2066efb9bc5f879a44
Chathuranga Nanayakkara <ChathurangaN@embla.lk>
2019-10-03 14:37:54 +0530

    image data generator

---
+++ natural.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow_estimator.python.estimator.canned.timeseries import model
+
+print(tf.__version__)
+
+TRAIN_DIR = 'C:/Users/chathurangan.EMBLA/Desktop/New folder/Natural/natural_dataset'
+
+class_names = ['airplane', 'car', 'cat', 'dog', 'flower', 'fruit', 'motorbike', 'person']
+
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   )
+train_generator = train_datagen.flow_from_directory(
+    TRAIN_DIR,
+    target_size=(28, 28),
+    batch_size=20,
+    class_mode='binary',
+    subset='training'
+)
+validation_generator = train_datagen.flow_from_directory(
+    TRAIN_DIR,
+    target_size=(28,28),
+    batch_size=15,
+    class_mode='binary',
+    subset='validation'
+
+)
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch = train_generator.samples,
+    validation_data = validation_generator,
+    validation_steps = validation_generator.samples,
+    epochs = 80)
